a1ebd916dcb881e08e41c8182cfc71447b4d19ac
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2024-10-27 12:50:59 -0500

    First commit

---
+++ app/src/main/java/com/quispe/angelo/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.quispe.angelo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.quispe.angelo.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    private var listPokemon: List<PokemonResponse> = emptyList()
+
+    private val adapter by lazy { PokemonAdapter(listPokemon) }
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvTinderPokemon.adapter = adapter
+        getAllPokemons()
+    }
+
+    private fun getRetrofit(): Retrofit {
+        val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
+            val newRequest = chain.request().newBuilder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("NAME-HEADER", "VALUE-HEADER")
+                .build()
+            chain.proceed(newRequest)
+        }
+
+        val client = httpClient.build()
+
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    private fun getAllPokemons() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val request = getRetrofit().create(PokemonApi::class.java).getPokemons()
+            if (request.isSuccessful) {
+                request.body()?.let {
+                    runOnUiThread {
+                        adapter.list = it.results
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+    }
+}
